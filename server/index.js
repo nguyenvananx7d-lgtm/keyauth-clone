f41b91f2361453410aa24fa708991f9531b2a62b
nguyenvananx7d-lgtm <nguyenvananx7d-lgtm@users.noreply.github.com>
2026-09-04 01:26:48 +0700

    Fix db import in client routes and Express 5 wildcard route

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.use('/api/client', clientRoutes)
 if (isProd) {
   const distPath = path.join(__dirname, '..', 'dist')
   app.use(express.static(distPath))
-  app.get('*splat', (_req, res) => {
+  app.get('/*splat', (_req, res) => {
     res.sendFile(path.join(distPath, 'index.html'))
   })
 }
